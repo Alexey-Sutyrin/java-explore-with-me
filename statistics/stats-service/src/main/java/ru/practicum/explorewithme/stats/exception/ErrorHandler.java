9008f2b312550c19a98ecebcdb5e69e816687c83
Alexey <vook-alf@yandex.ru>
2024-05-11 13:19:43 +0500

    Ya Praktikum Sprint 18 - Stage 2 - ewm-main-service

---
+++ statistics/stats-service/src/main/java/ru/practicum/explorewithme/stats/exception/ErrorHandler.java
@@ -0,0 +1,16 @@
+package ru.practicum.explorewithme.stats.exception; // changed to ErrorResponse instead of Map to fix issue
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleStatisticValidationException(StatisticValidationException e) {
+        return new ErrorResponse("Validation for statistic failed: " + e.getMessage());
+    }
+}
