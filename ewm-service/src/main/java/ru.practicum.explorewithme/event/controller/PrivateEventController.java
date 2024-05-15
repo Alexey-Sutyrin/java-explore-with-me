429bd6760814ca980988fcc3601ee19f499f8932
Alexey <vook-alf@yandex.ru>
2024-05-16 02:08:44 +0500

    Stage 2 - ewm-stats-service addition - main_svc addition Correction 6 - 10 of 10 checkpoints - EventController change

---
+++ ewm-service/src/main/java/ru.practicum.explorewithme/event/controller/PrivateEventController.java
@@ -0,0 +1,46 @@
+package ru.practicum.explorewithme.event.controller; //Private endpoints
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.explorewithme.StatisticClient;
+import ru.practicum.explorewithme.event.dto.*;
+import ru.practicum.explorewithme.event.service.EventService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class PrivateEventController {
+
+    private final EventService eventService;
+    private final StatisticClient statisticClient;
+
+    @PostMapping(value = "/users/{userId}/events")
+    @ResponseStatus(HttpStatus.CREATED)
+    public EventFullDto addEvent(@PathVariable Long userId, @Valid @RequestBody NewEventDto newEventDto) {
+        return eventService.addEvent(userId, newEventDto);
+    }
+
+    @GetMapping("/users/{userId}/events")
+    public List<EventShortDto> findEventsOfUser(@PathVariable Long userId,
+                                                @RequestParam(required = false, defaultValue = "0") Integer from,
+                                                @RequestParam(required = false, defaultValue = "10") Integer size) {
+        return eventService.findEventsOfUser(userId, from, size);
+    }
+
+    @GetMapping("/users/{userId}/events/{eventId}")
+    public EventFullDto findUserEventById(@PathVariable Long userId, @PathVariable Long eventId) {
+        return eventService.findUserEventById(userId, eventId);
+    }
+
+    @PatchMapping(value = "/users/{userId}/events/{eventId}")
+    public EventFullDto userUpdateEvent(@PathVariable Long userId,
+                                        @PathVariable Long eventId,
+                                        @Valid @RequestBody UpdateEventUserRequest updateEventUserRequest) {
+        return eventService.userUpdateEvent(userId, eventId, updateEventUserRequest);
+    }
+}
--- ewm-service/src/main/java/ru.practicum.explorewithme/event/controller/PublicEventController.java
+++ ewm-service/src/main/java/ru.practicum.explorewithme/event/controller/PublicEventController.java
@@ -1,8 +1,7 @@
-package ru.practicum.explorewithme.event.controller;
+package ru.practicum.explorewithme.event.controller; //Public endpoints
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.explorewithme.StatisticClient;
@@ -24,55 +23,10 @@ import static ru.practicum.explorewithme.constant.Constant.TIME_FORMAT;
 @Validated
 @RestController
 @RequiredArgsConstructor
-public class EventController {
+public class PublicEventController {
 
     private final EventService eventService;
     private final StatisticClient statisticClient;
-
-    //Private endpoints
-    @PostMapping(value = "/users/{userId}/events")
-    @ResponseStatus(HttpStatus.CREATED)
-    public EventFullDto addEvent(@PathVariable Long userId, @Valid @RequestBody NewEventDto newEventDto) {
-        return eventService.addEvent(userId, newEventDto);
-    }
-
-    @GetMapping("/users/{userId}/events")
-    public List<EventShortDto> findEventsOfUser(@PathVariable Long userId,
-                                                @RequestParam(required = false, defaultValue = "0") Integer from,
-                                                @RequestParam(required = false, defaultValue = "10") Integer size) {
-        return eventService.findEventsOfUser(userId, from, size);
-    }
-
-    @GetMapping("/users/{userId}/events/{eventId}")
-    public EventFullDto findUserEventById(@PathVariable Long userId, @PathVariable Long eventId) {
-        return eventService.findUserEventById(userId, eventId);
-    }
-
-    @PatchMapping(value = "/users/{userId}/events/{eventId}")
-    public EventFullDto userUpdateEvent(@PathVariable Long userId,
-                                        @PathVariable Long eventId,
-                                        @Valid @RequestBody UpdateEventUserRequest updateEventUserRequest) {
-        return eventService.userUpdateEvent(userId, eventId, updateEventUserRequest);
-    }
-
-    //Admin endpoints
-    @GetMapping("/admin/events")
-    public List<EventFullDto> findEventsByAdmin(@RequestParam(required = false) List<Long> users,
-                                                @RequestParam(required = false) List<String> states,
-                                                @RequestParam(required = false) List<Long> categories,
-                                                @RequestParam(required = false) @DateTimeFormat(pattern = TIME_FORMAT) LocalDateTime rangeStart,
-                                                @RequestParam(required = false) @DateTimeFormat(pattern = TIME_FORMAT) LocalDateTime rangeEnd,
-                                                @RequestParam(required = false, defaultValue = "0") Integer from,
-                                                @RequestParam(required = false, defaultValue = "10") Integer size) {
-        EventAdminParam eventAdminParam = new EventAdminParam(users, states, categories, rangeStart, rangeEnd, from, size);
-        return eventService.findEventsByAdmin(eventAdminParam);
-    }
-
-    @PatchMapping(value = "/admin/events/{eventId}")
-    public EventFullDto adminUpdateEvent(@PathVariable Long eventId,
-                                         @Valid @RequestBody UpdateEventAdminRequest updateRequest) {
-        return eventService.adminUpdateEvent(eventId, updateRequest);
-    }
 
     //Public endpoints
     @GetMapping("/events")
@@ -115,3 +69,4 @@ public class EventController {
         return eventService.changeEventRequestsStatus(userId, eventId, updateRequest);
     }
 }
+
