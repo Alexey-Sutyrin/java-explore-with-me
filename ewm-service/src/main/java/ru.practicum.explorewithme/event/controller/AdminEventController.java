d3350c74c8373ed1788f365dcb9bc49395ed825c
Alexey <vook-alf@yandex.ru>
2024-05-21 00:55:48 +0300

    Sprint 18 - Stage 3 - feature_subscriptions - readme update

---
+++ ewm-service/src/main/java/ru.practicum.explorewithme/event/controller/AdminEventController.java
@@ -0,0 +1,42 @@
+package ru.practicum.explorewithme.event.controller; //3
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.explorewithme.StatisticClient;
+import ru.practicum.explorewithme.event.dto.*;
+import ru.practicum.explorewithme.event.service.EventService;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static ru.practicum.explorewithme.constant.Constant.TIME_FORMAT;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class AdminEventController {
+
+    private final EventService eventService;
+    private final StatisticClient statisticClient;
+
+    @GetMapping("/admin/events")
+    public List<EventFullDto> findEventsByAdmin(@RequestParam(required = false) List<Long> users,
+                                                @RequestParam(required = false) List<String> states,
+                                                @RequestParam(required = false) List<Long> categories,
+                                                @RequestParam(required = false) @DateTimeFormat(pattern = TIME_FORMAT) LocalDateTime rangeStart,
+                                                @RequestParam(required = false) @DateTimeFormat(pattern = TIME_FORMAT) LocalDateTime rangeEnd,
+                                                @RequestParam(required = false, defaultValue = "0") Integer from,
+                                                @RequestParam(required = false, defaultValue = "10") Integer size) {
+        EventAdminParam eventAdminParam = new EventAdminParam(users, states, categories, rangeStart, rangeEnd, from, size);
+        return eventService.findEventsByAdmin(eventAdminParam);
+    }
+
+    @PatchMapping(value = "/admin/events/{eventId}")
+    public EventFullDto adminUpdateEvent(@PathVariable Long eventId,
+                                         @Valid @RequestBody UpdateEventAdminRequest updateRequest) {
+        return eventService.adminUpdateEvent(eventId, updateRequest);
+    }
+}
